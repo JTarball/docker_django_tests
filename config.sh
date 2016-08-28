e7d99954e2631ce39d2991154272ee3c87ab55c2
James Tarball <james.tarball@gmail.com>
2016-08-28 18:38:51 +0100

    first commit

---
+++ config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Copy of config.sh from official library + custom tests
+# Can't seem to get two config files working
+# Will need to update this periodically
+
+set -e
+
+globalTests+=(
+	gmt
+	#django
+)
+
+testAlias+=(
+
+)
+
+imageTests+=(
+	[$APP_TEST_IMAGE]='
+		python-imports
+		python-sqlite3
+	'
+)
+
+globalExcludeTests+=(
+	# single-binary images
+	[${APP_TEST_IMAGE}_utc]=1
+)
